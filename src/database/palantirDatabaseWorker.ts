e5c40d64483917c18a5c5cfb1f6862b8ab7d1dd8
tobeh <tobiasch@gmx.at>
2021-12-30 02:42:23 +0100

    fix returning an error resulting as possible null return

--- src/database/palantirDatabaseWorker.ts
+++ src/database/palantirDatabaseWorker.ts
@@ -8,8 +8,6 @@ import PalantirDatabase from "./palantirDatabase";
  */
 let database: PalantirDatabase | null;
 
-const error = () => { throw new Error("Database is not initialized!");}
-
 const palantirDatabaseWorker = {
 
     /** Inits a {@link PalantirDatabase} in a worker*/
@@ -19,62 +17,74 @@ const palantirDatabaseWorker = {
 
     /** Executes {@link PalantirDatabase.clearVolatile} in a worker*/
     clearVolatile() {
-        return database ? database.clearVolatile() : error();
+        if(!database) throw new Error("Database is not initialized.");
+        return database.clearVolatile();
     },
 
     /** Executes {@link PalantirDatabase.getActiveLobbies} in a worker*/
     getActiveLobbies() {
-        return database ? database.getActiveLobbies() : error();
+        if(!database) throw new Error("Database is not initialized.");
+        return database.getActiveLobbies();
     },
 
     /** Executes {@link PalantirDatabase.getDrop} in a worker*/
     getDrop(id: number = 0) {
-        return database ? database.getDrop(id): error();
+        if(!database) throw new Error("Database is not initialized.");
+        return database.getDrop(id);
     },
 
     /** Executes {@link PalantirDatabase.getLoginFromAccessToken} in a worker*/
     getLoginFromAccessToken(accessToken: string) {
-        return database ? database.getLoginFromAccessToken(accessToken) : error;
+        if(!database) throw new Error("Database is not initialized.");
+        return database.getLoginFromAccessToken(accessToken);
     },
 
     /** Executes {@link PalantirDatabase.getPublicData} in a worker*/
     getPublicData() {
-        return database ? database.getPublicData() : error;
+        if(!database) throw new Error("Database is not initialized.");
+        return database.getPublicData();
     },
 
     /** Executes {@link PalantirDatabase.getUserByLogin} in a worker*/
     getUserByLogin(login: number) {
-        return database ? database.getUserByLogin(login) : error;
+        if(!database) throw new Error("Database is not initialized.");
+        return database.getUserByLogin(login);
     },
 
     /** Executes {@link PalantirDatabase.isPalantirLobbyOwner} in a worker*/
     isPalantirLobbyOwner(lobbyID: string, lobbyPlayerID: number) {
-        return database ? database.isPalantirLobbyOwner(lobbyID, lobbyPlayerID) : error;
+        if(!database) throw new Error("Database is not initialized.");
+        return database.isPalantirLobbyOwner(lobbyID, lobbyPlayerID);
     },
 
     /** Executes {@link PalantirDatabase.rewardDrop} in a worker*/
     rewardDrop(login: string, eventdrop: number) {
-        return database ? database.rewardDrop(login, eventdrop) : error;
+        if(!database) throw new Error("Database is not initialized.");
+        return database.rewardDrop(login, eventdrop);
     },
 
     /** Executes {@link PalantirDatabase.setLobby} in a worker*/
     setLobby(id: string, key: string, description?: string, restriction?: string) {
-        return database ? database.setLobby(id, key, description, restriction) : error;
+        if(!database) throw new Error("Database is not initialized.");
+        return database.setLobby(id, key, description, restriction);
     },
 
     /** Executes {@link PalantirDatabase.setUserSprites} in a worker*/
     setUserSprites(login: number, sprites: string) {
-        return database ? database.setUserSprites(login, sprites) : error;
+        if(!database) throw new Error("Database is not initialized.");
+        return database.setUserSprites(login, sprites);
     },
 
     /** Executes {@link PalantirDatabase.writePlayerStatus} in a worker*/
     writePlayerStatus(status: types.playerStatus, session: string) {
-        return database ? database.writePlayerStatus(status, session) : error;
+        if(!database) throw new Error("Database is not initialized.");
+        return database.writePlayerStatus(status, session);
     },
 
     /** Executes {@link PalantirDatabase.writeReport} in a worker*/
     writeReport(lobbies: types.reportLobby[]) {
-        return database ? database.writeReport(lobbies) : error;
+        if(!database) throw new Error("Database is not initialized.");
+        return database.writeReport(lobbies);
     }
 }
 
