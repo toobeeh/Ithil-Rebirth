fadfcfb4177137e995f9ae80cf05d7b3867e9966
tobeh <tobiasch@gmx.at>
2022-06-18 23:37:49 +0200

    enable drop leagues

--- dist/drops.js
+++ dist/drops.js
@@ -63,7 +63,7 @@ class Drops {
                         const claimTarget = (await this.db.getDrop(nextDrop.DropID));
                         if (claimTarget.result && claimTarget.result.CaughtLobbyPlayerID == "") {
                             /* detect if it was caught below 1s => leaguedrop */
-                            let leagueDrop = false; //lastClaim.claimTimestamp - dispatchStats.dispatchTimestamp < 1000;
+                            let leagueDrop = lastClaim.claimTimestamp - dispatchStats.dispatchTimestamp < 1000;
                             /* time if league drop */
                             let leagueTime = leagueDrop ? lastClaim.claimTimestamp - dispatchStats.dispatchTimestamp : 0;
                             // claim and reward drop
