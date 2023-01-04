7687ce9e8dc54f299c32dd11ffe909b136647257
tobeh <tobiasch@gmx.at>
2023-01-04 19:18:35 +0100

    test special drops

--- dist/typoClient.js
+++ dist/typoClient.js
@@ -84,7 +84,7 @@ class TypoClient {
                     }
                 }
                 dropSpecial();
-            }, 1000 * 60 * 20 + 1000 * 60 * 40 * Math.random());
+            }, 10 * 10000 /* 1000 * 60 * 20 + 1000 * 60 * 40 * Math.random() */);
         };
         dropSpecial();
     }
