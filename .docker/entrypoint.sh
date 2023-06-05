02b1bf0b1fb116f7631ceba63bad3888dcb497db
tobeh <tobiasch@gmx.at>
2023-06-06 00:28:42 +0200

    remove ssl

--- .docker/entrypoint.sh
+++ .docker/entrypoint.sh
@@ -2,8 +2,5 @@
 set -e
 sed -i "s/DB_DOMAIN_NAME_SED_PLACEHOLDER/$DB_DOMAIN_NAME/g" /app/ecosystem.config.js 
 
-certbot certonly --standalone --agree-tos --email $LETSENCRYPT_EMAIL --domain $SOCKET_DOMAIN_NAME --non-interactive --keep-until-expiring
-crond
-
 # Start the main process
 exec "$@"
