a3d052ae7c797827a7aec988a0f562c9eb3117fc
tobeh <tobiasch@gmx.at>
2024-05-13 16:06:28 +0200

    debug log

--- src/workerServer.ts
+++ src/workerServer.ts
@@ -97,9 +97,10 @@ portscanner.findAPortNotInUse(
         // listen to ipc lobbies update event
         ipcClient.onActiveLobbiesChanged = (data) => {
             workerCache.activeLobbies = data.activeLobbies;
-            console.log("worker received lobbies: " + data.activeLobbies.length);
             const guildsDictionary: {[id: string]: guildLobbyLink[]} = {}
             data.activeLobbies.forEach(link => guildsDictionary[link.guildId] ? guildsDictionary[link.guildId].push(link) : guildsDictionary[link.guildId] = [link]);
+
+            console.log("worker received lobbies for guilds: " + Object.keys(guildsDictionary).join(", "));
 
             Object.keys(guildsDictionary).forEach(guild => {
 
